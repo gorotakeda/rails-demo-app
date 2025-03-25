c74def81158dd8d2187af890b89a739c7eefa05c
goro takeda <rugbytkg3@gmail.com>
2025-03-25 17:58:16 +0900

    seed作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,29 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# 御祈祷の種類
+prayer_types = [
+  { name: '七五三', description: '七五三のお祝い', price: 5000 },
+  { name: '安産祈願', description: '安産のご祈祷', price: 7000 },
+  { name: '交通安全', description: '交通安全のご祈祷', price: 5000 },
+  { name: '学業成就', description: '学業成就のご祈祷', price: 5000 }
+]
+
+prayer_types.each do |prayer|
+  PrayerType.find_or_create_by!(name: prayer[:name]) do |pt|
+    pt.attributes = prayer
+  end
+end
+
+# 予約枠
+(9..16).each do |hour|
+  TimeSlot.find_or_create_by!(
+    start_time: "#{hour}:00",
+    capacity: 2
+  )
+  
+  TimeSlot.find_or_create_by!(
+    start_time: "#{hour}:30",
+    capacity: 2
+  )
+end
